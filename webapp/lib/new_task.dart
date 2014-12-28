9ea51c040c19fb2df2a9d927dbe0a3e37f3038b8
martin.haesler <martin.haesler@gmail.com>
2014-12-28 11:01:33 +0000

    Store tasks in local storage

--- webapp/lib/new_task.dart
+++ webapp/lib/new_task.dart
@@ -1,14 +1,18 @@
 library new_task;
 
 import 'dart:html';
+import 'dart:convert';
 
 initNewTask() {
     var newTaskButton = querySelector('#showNewTask');
     newTaskButton.onClick.listen(showNewTask);
     InputElement dateinp = querySelector('#dateinp');
+    var saveTaskButton = querySelector('#savetask');
+    saveTaskButton.onClick.listen(saveTask);
 
     var cal = querySelector('.calendar');
     DateTime today = new DateTime.now();
+    print(today.millisecondsSinceEpoch);
     String todaystring = today.toString();
     dateinp.value = formatDate(todaystring);
     dateinp.onClick.listen(showCalendar);
@@ -18,18 +22,19 @@ initNewTask() {
 String formatDate(String date) {
     var spl = date.split('-');
     var day = spl[2].split(' ')[0];
-    
+
     return day+"/" +spl[1] +"/" + spl[0];
 }
 void showCalendar(_) {
-  var calview = querySelector('#calview');
-  if (calview.style.display == 'none') {
-    calview.style.display = '';
-    
-  } else {
-    calview.style.display = 'none';
-  }
+    var calview = querySelector('#calview');
+    if (calview.style.display == 'none') {
+        calview.style.display = '';
+
+    } else {
+        calview.style.display = 'none';
+    }
 }
+
 
 void showNewTask(_) {
     var newTaskHtml = querySelector('#newTask');
@@ -38,4 +43,29 @@ void showNewTask(_) {
     } else {
         newTaskHtml.style.display = 'none';
     }
+}
+void saveTask(_) {
+    String sum = querySelector('#tsummary').value;
+    String con = querySelector('#tcontent').value;
+    String sched = querySelector('#dateinp').value;
+    String timestamp = new DateTime.now().millisecondsSinceEpoch.toString();
+    final String STORAGE_KEY = 'darttasks';
+    List tasks = [];
+    if(window.localStorage.containsKey(STORAGE_KEY)) {
+        tasks = JSON.decode(window.localStorage[STORAGE_KEY]);
+
+        // Decoding Error, should not happen
+        if(tasks == null) {
+            tasks = [];
+        }
+    }
+
+    var task = {
+        "summary": sum,
+        "content": con,
+        "scheduled": sched,
+        "id": timestamp
+    };
+    tasks.add(task);
+    window.localStorage[STORAGE_KEY] = JSON.encode(tasks);
 }
