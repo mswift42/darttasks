664b4afcde4ca2b9af96ce665744d30f79103620
martin.haesler <martin.haesler@gmail.com>
2014-12-29 20:13:32 +0000

    sort tasks by scheduled date

--- webapp/lib/list_tasks.dart
+++ webapp/lib/list_tasks.dart
@@ -10,8 +10,14 @@ UListElement taskList;
 void listTasks() {
   var tl = querySelector('#taskList');
   List tasks = JSON.decode(window.localStorage[STORAGE_KEY]);
+  sortTasks(tasks);
   print(tasks);
 
+}
+
+// sortTasks - sort taskobjects by 'scheduled' in increasing order.
+void sortTasks(List tasks) {
+  tasks.sort((a,b) => a['scheduled'].compareTo(b['scheduled']));
 }
 
 
