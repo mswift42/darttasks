16d9c3f29584f4bf35f66dce9d0e51e871ebe2d5
martin.haesler <martin.haesler@gmail.com>
2014-12-29 22:12:23 +0000

    list tasks

--- webapp/lib/list_tasks.dart
+++ webapp/lib/list_tasks.dart
@@ -5,13 +5,27 @@ import 'dart:convert';
 
 
 final String STORAGE_KEY = 'darttasks';
-UListElement taskList;
 
 void listTasks() {
-  var tl = querySelector('#taskList');
+  var tl = querySelector('#tasklist');
   List tasks = JSON.decode(window.localStorage[STORAGE_KEY]);
   sortTasks(tasks);
-  print(tasks);
+  for (var task in tasks) {
+    var singletask = new LIElement();
+    var tasksummarycontainer = new DivElement();
+    var tasksummary = new Element.tag('p');
+    var taskcontentcontainer = new DivElement();
+    var taskcontent = new PreElement();
+    tl.children.add(singletask);
+    singletask.children.add(tasksummary);
+    tasksummary.appendText(task['summary']);
+    singletask.append(taskcontentcontainer);
+    taskcontentcontainer.append(taskcontent);
+    taskcontent.appendText(task['content']);
+    
+    
+  }
+  
 
 }
 
