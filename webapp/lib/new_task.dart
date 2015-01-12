b506e31df0b82158b3de127a00874c9bf201072d
martin.haesler <martin.haesler@gmail.com>
2015-01-12 13:17:15 +0000

    show tasks after submit of new task

--- webapp/lib/new_task.dart
+++ webapp/lib/new_task.dart
@@ -9,9 +9,11 @@ initNewTask() {
   var newTaskButton = querySelector('#showNewTask');
   newTaskButton.onClick.listen(showNewTask);
   InputElement dateinp = querySelector('#dateinp');
-  var saveTaskButton = querySelector('#savetask');
-  saveTaskButton.onClick.listen(saveTask);
 
+
+  // saveTaskButton.onClick.listen(saveTask);
+  var submitButton = querySelector('#savetask');
+  submitButton.onClick.listen(submitTask);
   var cal = querySelector('.calendar');
   DateTime today = new DateTime.now();
   String todaystring = today.toString();
@@ -24,7 +26,7 @@ String formatDate(String date) {
   List spl = date.split('-');
   String day = spl[2].split(' ')[0];
 
-  return day+"/" +spl[1] +"/" + spl[0];
+  return day + "/" + spl[1] + "/" + spl[0];
 }
 
 void showCalendar(_) {
@@ -34,22 +36,59 @@ void showCalendar(_) {
 void showNewTask(_) {
   toggleDisplay('#newTask');
 }
+void toggleValidWarning() {
+  toggleDisplay('#validationwarning');
+}
+
 void saveTask(_) {
-  String sum = querySelector('#tsummary').value;
+  String sum = querySelector('#tsummary').value.trim();
   String con = querySelector('#tcontent').value;
   String sched = querySelector('#dateinp').value;
   String timestamp = new DateTime.now().millisecondsSinceEpoch.toString();
+  if (validateTask(sum)) {
+    var task = {
+      "summary": sum,
+      "content": con,
+      "scheduled": sched,
+      "id": timestamp
+    };
+    tasks.add(task);
+    window.localStorage[STORAGE_KEY] = JSON.encode(tasks);
 
-  var task = {
-    "summary": sum,
-    "content": con,
-    "scheduled": sched,
-    "id": timestamp
-  };
-  tasks.add(task);
-  window.localStorage[STORAGE_KEY] = JSON.encode(tasks);
+  } else {
+    toggleValidWarning();
+
+  }
 }
 
-bool validateTask(String summary) {
-  return summary != '';
+void submitTask(Event e) {
+  e.preventDefault();
+  print(window.localStorage[STORAGE_KEY]);
+  String sum = querySelector('#tsummary').value.trim();
+  String con = querySelector('#tcontent').value;
+  String sched = querySelector('#dateinp').value;
+
+  String timestamp = new DateTime.now().millisecondsSinceEpoch.toString();
+  print(validateTask(sum));
+  print(sum.length);
+  if (validateTask(sum)) {
+    var task = {
+      'summary': sum,
+      'content': con,
+      'scheduled': sched,
+      'id': timestamp
+    };
+    tasks.add(task);
+    window.localStorage[STORAGE_KEY] = JSON.encode(tasks);
+        toggleDisplay('#newTask');
+        removeAllTasks();
+        listTasks();
+
+  } else {
+    toggleValidWarning();
+  }
+}
+
+bool validateTask(String sumstring) {
+  return sumstring != '';
 }
